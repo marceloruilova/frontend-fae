6157e563c945ff88b681ada51c289698a2dc4de5
marceloruilova <marcelo.rui11@gmail.com>
2021-07-15 14:17:43 -0500

    FrontEnd-FAE Begin

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    indent: ["error", 2],
+    "react/jsx-filename-extension": [0],
+    "no-unused-vars": "warn",
+    camelcase: "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  SVGPathSegLinetoRel
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
